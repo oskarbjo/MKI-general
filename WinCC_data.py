8519c5817f7324afd69697f13de0405b8f8ce8b6
Oskar Bjorkqvist <oskar.bjorkqvist@cern.ch>
2020-03-20 10:54:27 +0100

    WinCC plot fixes

--- WinCC_data.py
+++ WinCC_data.py
@@ -1,7 +1,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
 from scipy.optimize import curve_fit
-
+from itertools import groupby
 
 class winCCData():
     
@@ -28,24 +28,37 @@ class winCCData():
             self.Vmeas.append(np.double(data[5,i]))
             self.pulseLength.append(np.double(data[11,i]))
             self.Npulses.append(np.double(data[14,i]))
-        self.timeHours = np.divide(self.timeSeconds,60)
+        self.timeHours = np.divide(self.timeSeconds,3600)
     
     def unfoldPulseCount(self):
-        for i in range(0,len(self.Npulses)-1):
-            if self.Npulses[i] < self.Npulses[i+1]:
-                self.Npulses[i+1:] = self.Npulses[i+1:] + self.Npulses[i] - self.Npulses[i+1] + 1
-            
-    
+        
+        self.Npulses = self.Npulses - self.Npulses[0]
+        N=len(self.Npulses)
+        C = 0
+        for i in range(0,N-1):
+            if self.Npulses[i+1] < self.Npulses[i]:
+                C = C + self.Npulses[i] - self.Npulses[i+1] + 1
+                self.Npulses[i+1:] = self.Npulses[i+1:] + C
+            print(i)
+        
+        
+        
+#         self.uniquePulses = [k for k,g in groupby(self.Npulses) if k!=0]
+#         self.uniquePulseTimearrayHours = np.linspace(0,np.max(self.timeHours),len(self.uniquePulses))
+#         self.NuniquePulses = np.arange(0,np.max(self.uniquePulses))
+        
     def plotwinCCdata(self):
         figure, axis_1 = plt.subplots(num=None, figsize=(11, 6), dpi=140, facecolor='w', edgecolor='k')
         p1,=axis_1.semilogy(self.timeHours,self.vacuum,color='blue',label='Pressure')
         plt.ylabel('Pressure [Bar]')
         plt.xlabel('Time [Hours]')
         plt.xlim([0,np.max(self.timeHours)])
+#         plt.ylim([1e-11, 1e-5])
         axis_2 = axis_1.twinx()
         p2,=axis_2.plot(self.timeHours,self.Vref,color='green',label='Vref')
         p3,=axis_2.plot(self.timeHours,self.Vmeas,color='red',label='Vmeas')
         plt.ylabel('Voltage [kV]')
+        plt.ylim([25, 60])
         axis_3 = axis_1.twinx()
         axis_3.spines["right"].set_position(("axes", 1.051))
         p4,=axis_3.plot(self.timeHours,self.Npulses,label='Npulses')
@@ -75,7 +88,7 @@ def main():
 
     ###################### Plot winCC data: ######################
     # File path of exported winCC data (has to be tab delimited!)
-    winccPath = r"G:\Departments\TE\Groups\ABT\Sections\PPE\Users\objorkqv\winCC export\20Feb2020_23Feb2020.csv"
+    winccPath = r"G:\Departments\TE\Groups\ABT\Sections\PPE\Users\objorkqv\winCC export\2020-03-15_2020-03-18.csv"
     winCC = winCCData(winccPath)
     winCC.plotwinCCdata()
 #      winCC.vacuumExpFit()
