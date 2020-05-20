10110493200e368acc94aad20ab5942e3f618f4c
Oskar Bjorkqvist <oskar.bjorkqvist@cern.ch>
2020-05-20 11:31:56 +0200

    minor fix

--- WinCC_data.py
+++ WinCC_data.py
@@ -5,6 +5,8 @@ from itertools import groupby
 from os import listdir
 from os.path import isfile, join
 
+print('*** Running Script ***')
+
 class winCCData():
     
     def __init__(self,path):
@@ -90,7 +92,7 @@ class winCCData():
         self.VmeasFinal = np.round(self.VmeasPlot[-1],4)
         self.NpulsesFinal = self.NpulsesPlot[-1]
         
-    def plotwinCCdata(self,plotDateEveryN,vacuumMin,vacuumMax):
+    def plotwinCCdata(self,plotDateEveryN,vacuumMin,vacuumMax, Plotfilename):
         figure, axis_1 = plt.subplots(num=None, figsize=(11, 6), dpi=140, facecolor='w', edgecolor='k')
         p1,=axis_1.semilogy(self.timeHoursPlot,self.vacuumPlot,color='blue',label='Pressure')
         plt.ylim([vacuumMin,vacuumMax])
@@ -113,6 +115,11 @@ class winCCData():
         ax2.set_xticklabels(self.timeStampsPlot[0::plotDateEveryN],rotation=90)
         ax2.tick_params(labelsize=5)
         plt.xlim([np.min(self.timeHoursPlot),np.max(self.timeHoursPlot)])
+        plt.savefig(Plotfilename, bbox_inches='tight', dpi=200)
+        print('Plot Saved As: ',Plotfilename)
+        print('Total pulse count = ',self.NpulsesPlotFinal) # last element of list
+        print('Last demanded voltage = ',self.VrefFinal,' kV') # last element of list
+        print('Last reference voltage = ',self.VrefMeas,' kV') # last element of list
         
     def vacuumExpFit(self):
         t1=103772
@@ -148,14 +155,15 @@ winccPath = r"\\cern.ch\dfs\Departments\TE\Groups\ABT\Users\B\BJORKQVIST_Oskar\t
 #Time stamps (be mindful that the data set needs to contain the selected time stamps)
 timeStamp1 = '20/02/2020 07:00:00' #Start timestamp, must have format '20/02/2020 11:08:14'
 timeStamp2 = '26/02/2020 06:59:00' #End timestamp, must have format '20/02/2020 11:08:14'
+Plotfilename = 'WinCC_Plot_20022020-18032020.png' # Name of plot export
 winCC = winCCData(winccPath)        
 winCC.getDemandedDates(timeStamp1,timeStamp2)
 winCC.findSparks()
-dateTickEveryNseconds = 50000      #Plot a date tick every N seconds
+dateTickEveryNseconds = 50000      #Plot a date tick every 0.5*N seconds
 vacuumMin = 1e-11
 vacuumMax = 1e-6
-winCC.plotwinCCdata(dateTickEveryNseconds,vacuumMin,vacuumMax)
-print('Done')
+winCC.plotwinCCdata(dateTickEveryNseconds,vacuumMin,vacuumMax,Plotfilename)
+print('*** Done ***')
 #      winCC.vacuumExpFit()
 plt.show()
 
